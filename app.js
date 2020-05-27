041d416d71f627c69f5c8cf516828830d1394db7
anaamnizami <anaamnizamii@gmail.com>
2020-05-27 15:45:17 +0500

    cors headers

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ dotenv.config()
 //
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
