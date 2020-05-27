86c9a4bd7757bf5f8a3ae5ea8c77bb987f06e547
anaamnizami <anaamnizamii@gmail.com>
2020-05-27 15:56:53 +0500

    cors headersd

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ var cors = require('cors')
 
 const port = process.env.PORT || 4000
 dotenv.config()
-// app.use(cors())
+app.use(cors())
 
 //
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 //connect to db
 
